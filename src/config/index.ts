2ee86a7619613f75b4ffc5d35fb142e0af339b82
singcl <1276837689@qq.com>
2022-12-26 23:55:15 +0800

    chore(config): config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://192.168.2.101:8994';
-// export const BASE_URL = 'https://wx.imcoco.top';
+// export const BASE_URL = 'http://192.168.2.101:8994';
+export const BASE_URL = 'https://wx.imcoco.top';
